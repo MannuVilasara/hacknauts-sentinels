54394c2b30246237225b246eb4103249200f3174
MannuVilasara <mannuvilasara@gmail.com>
2025-11-16 05:50:23 +0530

    fix: ports

--- services/securebot/src/index.js
+++ services/securebot/src/index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json({ limit: '10mb' }));
